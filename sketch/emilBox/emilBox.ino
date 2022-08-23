97911ff7a9ef89bd2acd5544fd1931a9854e633a
Luis Pato <lpato@dfau.de>
2022-08-23 15:49:51 +0200

    wip: sketch wit working rfid and dfPlayer mini

--- sketch/emilBox/emilBox.ino
+++ sketch/emilBox/emilBox.ino
@@ -1,14 +1,28 @@
+/* I should try to remove the Arduino library and see if it still works */
+#include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include "DFRobotDFPlayerMini.h"
 
 // RFID Reader Pins
-#define SS_PIN  5  // ESP32 pin GIOP5 
-#define RST_PIN 27 // ESP32 pin GIOP27 
+#define RFID_CARD_SS_PIN  5  // ESP32 pin GIOP5 
+#define RFID_CARD_RST_PIN 27 // ESP32 pin GIOP27
 
 // RFID Reader variables
-MFRC522 rfidReader(SS_PIN, RST_PIN);
+MFRC522 rfidReader(RFID_CARD_SS_PIN, RFID_CARD_RST_PIN);
 uint8_t control = 0x00;
 uint8_t rfidReadInterval = 100;
+
+// SD Card variables
+HardwareSerial mySoftwareSerial(1);
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+// Volume variables
+int VOLUME = 15;
+int VOLUME_MAX = 30;
+int VOLUME_MIN = 0;
+int VOLUME_CHANGE_AMOUNT = 1;
 
 // Tag ids
 String TAG_TEST =           "93 44 5C 92";
@@ -37,12 +51,57 @@ int FOLDER_CHILDREN =      9;
 ***     SETUP                                                                           ***
 ******************************************************************************************/
 void setup() {
+  // Start communication with the SD Card
+  mySoftwareSerial.begin(9600, SERIAL_8N1, 16, 17);  // speed, type, RX, TX
+  // Start serial communication
   Serial.begin(115200);
 
   // RFID Reader setup
   SPI.begin(); // init SPI bus
   rfidReader.PCD_Init(); // init RFID reader
   Serial.println("RFID Reader is ready...");
+
+  // SD Card reader setup
+  Serial.println();
+    Serial.println(F("DFRobot DFPlayer Mini Demo"));
+    Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+    if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+
+      Serial.println(myDFPlayer.readType(),HEX);
+      Serial.println(F("Unable to begin:"));
+      Serial.println(F("1.Please recheck the connection!"));
+      Serial.println(F("2.Please insert the SD card!"));
+      while(true);
+    }
+    Serial.println(F("DFPlayer Mini online."));
+
+    myDFPlayer.setTimeOut(500); //Set serial communication time out 500ms
+
+    //----Set volume----
+    myDFPlayer.volume(30);  //Set volume value (0~30).
+    myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+    myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+
+    int delayms=100;
+
+    //----Read imformation----
+    Serial.println(F("readState--------------------"));
+    Serial.println(myDFPlayer.readState()); //read mp3 state
+    Serial.println(F("readVolume--------------------"));
+    Serial.println(myDFPlayer.readVolume()); //read current volume
+    Serial.println(F("readEQ--------------------"));
+    Serial.println(myDFPlayer.readEQ()); //read EQ setting
+    Serial.println(F("readFileCounts--------------------"));
+    Serial.println(myDFPlayer.readFileCounts()); //read all file counts in SD card
+    Serial.println(F("readCurrentFileNumber--------------------"));
+    Serial.println(myDFPlayer.readCurrentFileNumber()); //read current play file number
+    Serial.println(F("readFileCountsInFolder--------------------"));
+    Serial.println(myDFPlayer.readFileCountsInFolder(3)); //read fill counts in folder SD:/03
+    Serial.println(F("--------------------"));
+
+    Serial.println(F("myDFPlayer.play(1)"));
+    // myDFPlayer.play(1);  //Play the first mp3
 }
 
 
@@ -68,7 +127,7 @@ String getTagUid() {
 void checkTagValidity(String tag_uid) {
   if (tag_uid == TAG_TEST) {
     Serial.println("BLUE TAG");
-    /*myDFPlayer.playFolder(1, 1);*/
+    myDFPlayer.playFolder(1, 1);
   } else if (tag_uid == TAG_BACH) {
     Serial.println("BACH");
   } else {
@@ -127,8 +186,11 @@ void loop() {
 
   Serial.println("Card Removed");
 
-  delay(rfidReadInterval); // Interval at which to read cards (maybe we should turn it into variable)
+  delay(rfidReadInterval); // Interval at which to read cards
 
   rfidReader.PICC_HaltA();
   rfidReader.PCD_StopCrypto1();
+
+  // Stop music play when no more RFID
+  myDFPlayer.stop();
 }
